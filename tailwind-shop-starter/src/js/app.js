806e3449b5595b782332bb6b3c59e7277c46538f
emrecan.y <emrecanyilmaz1997@gmail.com>
2024-12-02 10:29:16 +0100

    initial commit of existing template

---
+++ tailwind-shop-starter/src/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Injects HTML from a file into an element
+ * @param {string} htmlFile - The path to the HTML file
+ * @param {string} element - The element to inject the HTML into
+ * @returns {void}
+ * @example
+ * injectHTML('path/to/file.html', '.element')
+ */
+function injectHTML(htmlFile, element) {
+  fetch(htmlFile)
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector(element).innerHTML = data;
+    });
+}
+
+// Inject the header
+injectHTML("components/header/header.html", "#header");
+injectHTML("components/products/products.html", "#products");
