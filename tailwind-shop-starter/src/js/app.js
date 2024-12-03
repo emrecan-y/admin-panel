dd3a5c21af3384dddcd6af66bd81c381cdf61709
emrecan.y <emrecanyilmaz1997@gmail.com>
2024-12-03 14:50:51 +0100

    impelement temporary solution for fetching products

--- tailwind-shop-starter/src/js/app.js
+++ tailwind-shop-starter/src/js/app.js
@@ -14,6 +14,32 @@ function injectHTML(htmlFile, element) {
     });
 }
 
+function injectProducts(htmlFile, element) {
+  fetch(htmlFile)
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector(element).innerHTML = data;
+      const table = document.getElementById("products-table");
+      const firstRow = document.querySelector("#products-table th");
+      fetch("http://localhost:3000/products").then((response) =>
+        response.json().then((response) =>
+          response.data.forEach((product) => {
+            const row = table.insertRow();
+            row.classList.add(...firstRow.classList);
+            const id = row.insertCell(0);
+            id.innerHTML = product.id;
+            const name = row.insertCell(1);
+            name.innerHTML = product.name;
+            const description = row.insertCell(2);
+            description.innerHTML = product.description;
+            const price = row.insertCell(3);
+            price.innerHTML = product.price;
+          }),
+        ),
+      );
+    });
+}
+
 // Inject the header
 injectHTML("components/header/header.html", "#header");
-injectHTML("components/products/products.html", "#products");
+injectProducts("components/products/products.html", "#products");
