1ddfc2ecb5be1c4aa7c143a4fd0744637260bae5
emrecan-y <emrecanyilmaz1997@gmail.com>
2024-12-05 12:51:36 +0100

    add colors

--- tailwind-shop-starter/tailwind.config.js
+++ tailwind-shop-starter/tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         nav: "#212121",
         page: "#0F0F0F",
+        "table-light" :"#545454",
+        "table-dark" :"#3E3E3E",
+        "accent-dark": "#7C309C",
+        "accent-light": "#C847FF",
       },
     },
   },
